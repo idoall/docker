61042c8d126b5e4b6679bfef1be30283ce628056
idoall <lion.888@gmail.com>
2022-01-29 01:01:19 +0800

    expressvpn add new command

--- expressvpn/3.17/checkExpressVPNStatus.sh
+++ expressvpn/3.17/checkExpressVPNStatus.sh
@@ -6,6 +6,10 @@ if [[ $check_results =~ "Unable to connect" ]]
 then
     echo "[$time]Unable to connect.. "
     expressvpn connect
+elif [[ $check_results =~ "Not connected" ]]
+then
+    echo "[$time]Not connected.. "
+    expressvpn connect
 else
 	connectStr=`echo $check_results | sed -n '1p'`
 	echo "[$time]$connectStr"
